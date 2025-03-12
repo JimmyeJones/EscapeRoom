37850c61ba5d7f4e2b8fc5e70f59a4e3ae986a93
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2025-03-12 18:16:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,16 @@ words = ["tree", "ocean", "boat"]
 st.session_state["solved"] = []
 def scramble(a):
     return string_utils.shuffle(a)
+def is_in_ss(index, list):
+    included = False
+    for all in list:
+        if index in all:
+            return True
+    if not included:         
+        return False
+        
 for scrambled1 in scrambled:
-    if scrambled.index(scrambled1) not in st.session_state["solved"]:
+    if is_in_ss(scrambled.index(scrambled1), st.session_state["solved"]):
         if st.text_input(scrambled1) == words[scrambled.index(scrambled1)]:
             if scrambled.index(scrambled1) not in st.session_state["solved"]:
                 st.session_state["solved"].append(scrambled.index(scrambled1))
