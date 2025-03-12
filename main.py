27d29b64666e0d1e6cad34dd84e8b25e9396f303
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2025-03-12 18:32:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import string_utils
 st.title("Unscramble")
-scrambled = ["tere", "eonac", "otab"]
 words = ["tree", "ocean", "boat"]
 st.session_state["solved"] = []
 st.session_state["scrambled"] = []
@@ -10,6 +9,7 @@ def scramble(a):
 if len(st.session_state["scrambled"]) == 0:
     for word in words:
         st.session_state["scrambled"].append(scramble(word))
+scrambled = st.session_state["scrambled"]
         
 for scrambled1 in scrambled:
     
