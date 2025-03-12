bfc5ea888841e90f14ad9bcd9a25f55d619f8f8e
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2025-03-12 18:02:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import string_utils
 st.title("Unscramble")
 scrambled = ["tere", "eonac", "otab"]
 words = ["tree", "ocean", "boat"]
-st.session_state["solved"] = []
+st.session_state["solved"]{0} = []
 def scramble(a):
     return string_utils.shuffle(a)
 for scrambled1 in scrambled:
-    if scrambled.index(scrambled1) not in st.session_state["solved"]:
+    if scrambled.index(scrambled1) not in st.session_state["solved"]{0}:
         if st.text_input(scrambled1) == words[scrambled.index(scrambled1)]:
-            if scrambled.index(scrambled1) not in st.session_state["solved"]:
-                st.session_state["solved"].append(scrambled.index(scrambled1))
+            if scrambled.index(scrambled1) not in st.session_state["solved"]{0}:
+                st.session_state["solved"]{0}.append(scrambled.index(scrambled1))
     else:
         st.subheader(words[scrambled.index(scrambled1)])
-st.write(st.session_state["solved"])
+st.write(st.session_state["solved"]{0})
