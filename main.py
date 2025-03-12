77b7d47cbe97455f200f6c767bfd167ed9d5639b
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2025-03-12 15:55:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import streamlit as st
 import string_utils
 st.title("Unscramble")
 words = ["tree", "ocean"]
+word_num = 0
+word_input = []
 for word in words:
+    word_num += 1
     scrambled = string_utils.shuffle(word)
-    if st.text_input(scrambled) == "tree":
+    word_input[word_num] = st.text_input(scrambled)
+for word_i in word_input:
+    if word_i == words[word_input.len(word_i)]:
         st.text("Great job!!!")
