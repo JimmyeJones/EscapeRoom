19bab9ca245d5c7336710dbfe639a3c23435d529
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2025-03-12 16:13:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,16 @@ st.title("Unscramble")
 words = ["tree", "ocean"]
 def scramble(in):
     return string_utils.shuffle(in)
+try:
+    scrambled
+except NameError:
+    scrambled = []
+    for word in words:
+        scrambled.append(scramble(word))
+        
+for scrambled_word in scrambled:
+    solved = st.text_input(scrambled_word)
+    solved_words.append(solved)
+for solved_word in solved_words:
+    if solved_word == words[solved_words.len(solved_word)]:
+        st.text("Nice Job!!!!")
