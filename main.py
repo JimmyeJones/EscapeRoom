ead9f8047b39e11588ac0bc650fa9bf835cdf4d7
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2025-03-12 17:30:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import string_utils
 st.title("Unscramble")
 scrambled = ["tere", "eonac", "otab"]
 words = ["tree", "ocean", "boat"]
+st.session_state["solved"] = []
 try:
     ans
 except NameError:
@@ -10,6 +11,8 @@ except NameError:
 def scramble(a):
     return string_utils.shuffle(a)
 for scrambled1 in scrambled:
-    ans[scrambled.index(scrambled1)] = st.text_input(scrambled1)
+    if st.text_input(scrambled1) == words[scrambled.index(scrambled1)]:
+        if scrambled.index(scrambled1) not in st.session_state["solved"]:
+            st.session_state["solved"].append(scrambled.index(scrambled1))
 
-st.write(ans)
+st.write(st.session_state["solved"])
