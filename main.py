3f996b465cd55b49c73f2d2e61f2969b391bdf73
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2025-03-12 16:41:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import string_utils
 st.title("Unscramble")
 scrambled = ["tere", "eonac", "otab"]
 words = ["tree", "ocean", "boat"]
+try:
+    ans
+except NameError:
+    ans = []
 def scramble(a):
     return string_utils.shuffle(a)
 for scrambled1 in scrambled:
