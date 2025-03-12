eefac28a2e9aa5f2fbe1cb566662018d2dd38e8a
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2025-03-12 17:58:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ except NameError:
 def scramble(a):
     return string_utils.shuffle(a)
 for scrambled1 in scrambled:
-    if st.text_input(scrambled1) == words[scrambled.index(scrambled1)]:
-        if scrambled.index(scrambled1) not in st.session_state["solved"]:
-            st.session_state["solved"].append(scrambled.index(scrambled1))
-
+    if scrambled.index(scrambled1) not in st.session_state["solved"]:
+        if st.text_input(scrambled1) == words[scrambled.index(scrambled1)]:
+            if scrambled.index(scrambled1) not in st.session_state["solved"]:
+                st.session_state["solved"].append(scrambled.index(scrambled1))
+    else:
+        st.subheader(words[scrambled.index(scrambled1)])
