0f50fb4a58e575c0f8e718f425c6735b87787301
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2025-03-12 16:18:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ import streamlit as st
 import string_utils
 st.title("Unscramble")
 words = ["tree", "ocean"]
+try: 
+    solved
+except NameError:
+    solved = []
+    for x in words:
+        solved.append(False)
+    
 def scramble(in):
     return string_utils.shuffle(in)
+
 try:
     scrambled
 except NameError:
@@ -12,8 +20,9 @@ except NameError:
         scrambled.append(scramble(word))
         
 for scrambled_word in scrambled:
-    solved = st.text_input(scrambled_word)
-    solved_words.append(solved)
+    solved1 = st.text_input(scrambled_word)
+    solved_words.append(solved1)
 for solved_word in solved_words:
     if solved_word == words[solved_words.len(solved_word)]:
         st.text("Nice Job!!!!")
+        solved[solved_words.len(solved_word)] = True
