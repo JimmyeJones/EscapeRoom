8f83f27a1c4ffde5380b99b201ec954beafd793d
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2025-03-20 17:33:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -89,5 +89,4 @@ if show_code == True:
     st.title("The final code is:")
     st.subheader(st.secrets["final_code"])
 elif show_code == False:
-    st.error(f"Whoops! You have selected (an) incorrect answer(s). Error Code(s): {", ".join(error_codes1)}")
-
+    st.error(f"Whoops! You have selected (an) incorrect answer(s). Error Code(s): {", ".join(str(error_codes1))}")
