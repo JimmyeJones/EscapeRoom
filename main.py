62a745b9286e9e8ce1f881862913382ce1115fe4
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2025-03-20 17:36:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ for check in st.session_state["solved"]:
         show_code = True
 error_codes1 = []
 for error_code in error_codes:
-    code1 = error_code*5
+    code1 = int(error_code)*5
     error_codes1.append(code1)
 if show_code == True:
     st.title("The final code is:")
