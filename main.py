58c0d0fb6ed0ccb9982979197c6b1013e3ab1817
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2025-03-19 20:27:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ if True:   #for testing
         for word_in_sent in sentence.split():
             
             if word_in_sent in words:
-                st.session_state["sent_ans"][sentences.index(sentence)][words.index(word_in_sent)] = st.selectbox("", words, key=f"{sentences.index(sentence)}{words.index(word_in_sent)}")                 
+                st.session_state["sent_ans"][sentences.index(sentence)][sentence.index(word_in_sent)] = st.selectbox("", words, key=f"{sentences.index(sentence)}{sentence.index(word_in_sent)}")                 
             else:
                 st.write(word_in_sent)
         st.divider()
