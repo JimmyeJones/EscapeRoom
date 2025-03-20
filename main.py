e6748fe73ec1c58eca64fd42a49791b2cae52a6e
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2025-03-20 17:26:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,6 @@ if len(st.session_state["solved1"]) == len(scrambled):
         else:
             st.session_state["solved"][st.session_state["sent_ans"].index(sentence1)] = False
         st.write(sentences[st.session_state["sent_ans"].index(sentence1)].lower())
-st.write(st.session_state["solved"])
 error_codes = []
 show_code = ""
 for check in st.session_state["solved"]:
